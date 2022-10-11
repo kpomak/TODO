fbd803682519f4c55901dbb63959821bc3bb8b84
kpomak <romka_ne@mail.ru>
2022-10-11 22:51:02 +0300

    set up CSS

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -2,40 +2,40 @@ import React from "react";
 
 
 const userInstance = (user) => {
-    return (
-            <div className="tab_line" key={user.id}>
-                <div>
-                    {user.first_name}
-                </div>
-                <div>
-                    {user.last_name}
-                </div>
-                <div>
-                    {user.email}
-                </div>
-            </div>
-    );
+	return (
+		<div className="tab_line" key={user.id}>
+			<div className="tab_cell">
+				{user.first_name}
+			</div>
+			<div className="tab_cell">
+				{user.last_name}
+			</div>
+			<div className="tab_cell">
+				{user.email}
+			</div>
+		</div>
+	);
 };
 
 const usersList = (users) => {
-    return (      
-        <div className="users_tab">
-            <div className="tab_head">
-                <div>
-                    Имя
-                </div>
-                <div>
-                    Фамилия
-                </div>
-                <div>
-                    Возраст
-                </div>
-            </div>
-            {users.map(user => {
-                return userInstance(user);
-                })}
-        </div>       
-    );
+	return (      
+		<div className="tab_users">
+			<div className="tab_head">
+				<div className="tab_cell">
+					Имя
+				</div>
+				<div className="tab_cell">
+					Фамилия
+				</div>
+				<div className="tab_cell">
+					Email
+				</div>
+			</div>
+			{users.map(user => {
+				return userInstance(user);
+			})}
+		</div>       
+	);
 };
 
 export default usersList;

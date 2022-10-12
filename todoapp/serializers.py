2df31677b4b0f552c8a22440c058684364c97156
kpomak <romka_ne@mail.ru>
2022-10-12 16:31:36 +0300

    todo and project api

---
+++ todoapp/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+from todoapp.models import Project, ToDo
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            "project_name",
+            "link",
+            "description",
+            "project_team",
+        ]
+
+
+class ToDoModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = [
+            "project",
+            "user",
+            "body",
+            "is_active",
+        ]

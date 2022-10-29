373bb1ebbe9231ca9f08cf4676348cf3f8b833b8
kpomak <romka_ne@mail.ru>
2022-10-29 20:04:28 +0300

    merged lesson-9

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -14,3 +14,18 @@ class CustomUserModelSerializer(HyperlinkedModelSerializer):
             "last_name",
             "email",
         ]
+
+
+class CustomUserModelSerialiserFull(HyperlinkedModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "id",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "email",
+            "is_superuser",
+            "is_staff",
+        ]
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
@@ -12,13 +15,27 @@ router.register("users", CustomUserModelViewSet)
 router.register("projects", ProjectModelViewSet)
 router.register("todo", ToDoModelViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ToDo task list",
+        default_version="ver_1.0",
+        description="Documentation to ToDo-API",
+        contact=openapi.Contact(email="roman_ryzkov@internet.ru"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include(router.urls)),
-    path("api-token-auth/", views.obtain_auth_token),
+    path("api/", include(router.urls), name="api"),
+    path("api-token-auth/", views.obtain_auth_token, name="toke_auth"),
     path("api-jwt/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api-jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
